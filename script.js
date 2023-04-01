29228838004132f6c0255fa27767502dc3156394
Saikat Ghosh <saikatghosh5434@gmail.com>
2023-04-01 10:12:14 +0530

    add: item and delete: item functionality added.

--- script.js
+++ script.js
@@ -6,13 +6,22 @@ const input=document.querySelector('#item-input')
 //When I submit the form add a new element
 form.addEventListener('submit', e=>{
     e.preventDefault()
-})
-//1.Create a new item
-const item = document.createElement('div')
-item.innerText = input.value
-item.classList.add('list-item')
 
-//console.log(item)
+    //1.Create a new item
+const newItem = document.createElement('div')
+newItem.innerText = input.value
+newItem.classList.add("list-item")
+
+console.log(newItem)
 
 //2.Add that item to the list
-list.appendChild(item)
+list.appendChild(newItem)
+
+//3.Clear input after submitting on "Add Item" button
+input.value=""
+
+//4.Setup event listner to delete item when clicked
+newItem.addEventListener('click', e=>{
+    newItem.remove()
+})
+})
